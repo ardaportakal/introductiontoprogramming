a8c1e97566169b299d0552df1b9f726e5ea0b194
ardaportakal <portakal.arda@ibu.edu.mk>
2026-04-20 21:00:49 +0300

    Add files via upload

--- week1/mario.c
+++ week1/mario.c
@@ -1,20 +1,4 @@
-// mario.c
-// CS50x - Week 1, Problem Set 1
-//
-// Exercise: Mario (Less Comfortable)
-// ------------------------------------
-// Print a right-aligned half-pyramid of # symbols.
-// The user provides the height (1–8).
-//
-// Expected output (height = 4):
-//    #
-//   ##
-//  ###
-// ####
-//
-// How to compile:  make mario
-// How to run:      ./mario
-// How to check:    check50 cs50/problems/2024/x/mario/less
+//Arda portakal AIE İNT TO PROGRAMİNG HOMEWORK
 
 #include <cs50.h>
 #include <stdio.h>
@@ -23,25 +7,29 @@ int main(void)
 {
     int height;
 
-    // TODO: Use a do-while loop to prompt for height.
-    //       Keep re-prompting until height is between 1 and 8 (inclusive).
+
     do
     {
-        // TODO: Get an integer from the user with prompt "Height: "
-
+        height = get_int("Height: ");
     }
-    while (/* TODO: condition to keep looping */ false);
+    while (height < 1 || height > 8);
 
-    // TODO: Use a for loop to iterate over each row (1 through height).
+
     for (int row = 1; row <= height; row++)
     {
-        // TODO: Print (height - row) spaces using a for loop.
+
+        for (int space = 0; space < height - row; space++)
+        {
+            printf(" ");
+        }
 
 
-        // TODO: Print (row) hashes using a for loop.
+        for (int hash = 0; hash < row; hash++)
+        {
+            printf("#");
+        }
 
 
-        // Print a newline to end the row.
         printf("\n");
     }
 
